276b75e2401968bdb03738ff9bcd33460854c0b0
AmbinintsoaZo <herizo@nomade.mg>
2023-09-11 15:27:39 +0300

    initialize application module and package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.use(bodyParser.json({ limit: '10000mb' }));
+  app.use(bodyParser.urlencoded({ limit: '9000mb', extended: true }));
+
+  app.enableCors({
+    origin: ['https://localhost:4200', 'http://localhost:4200'],
+    allowedHeaders: [
+      'X-Requested-With',
+      'X-HTTP-Method-Override',
+      'Content-Type',
+      'Accept',
+      'Observe',
+      'authorization ',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
