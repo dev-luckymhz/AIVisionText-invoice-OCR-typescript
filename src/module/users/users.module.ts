a7850daa722061b5567eecd1407bc175e85b608d
AmbinintsoaZo <herizo@nomade.mg>
2023-09-12 09:36:39 +0300

    Setting Up Authentication module

--- src/module/users/users.module.ts
+++ src/module/users/users.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './users.service';
 import { UserController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UserController],
+  controllers: [UserController, AuthController],
   providers: [UserService],
 })
 export class UsersModule {}
