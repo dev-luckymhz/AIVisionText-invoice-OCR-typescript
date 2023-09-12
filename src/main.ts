be415b637324420fc88933086e8adbd91bfbc5e7
AmbinintsoaZo <herizo@nomade.mg>
2023-09-12 11:53:10 +0300

    Adding user role to the user Auth module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
+import fs from 'fs';
+import path from 'path';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const ssl = process.env.SSL === 'true';
+  let httpsOptions = null;
+  if (ssl) {
+    const keyPath = process.env.SSL_KEY_PATH || '';
+    const certPath = process.env.SSL_CERT_PATH || '';
+    httpsOptions = {
+      key: fs.readFileSync(path.join(__dirname, keyPath)),
+      cert: fs.readFileSync(path.join(__dirname, certPath)),
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.setGlobalPrefix('api');
   app.use(bodyParser.json({ limit: '10000mb' }));
@@ -28,6 +43,23 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('PRM- AIVisionText')
+    .setDescription('The PRM API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const hostname = process.env.HOSTNAME || 'localhost';
+  const port = Number(process.env.PORT) || 3000;
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(port, hostname, () => {
+    const address =
+      'http' + (ssl ? 's' : '') + '://' + hostname + ':' + port + '/';
+    Logger.log('Listening at ' + address);
+  });
 }
 bootstrap();
