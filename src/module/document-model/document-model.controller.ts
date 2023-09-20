ba09658286e96176e524539baeb354a25d76141f
AmbinintsoaZo <herizo@nomade.mg>
2023-09-20 14:42:20 +0300

    Fixig bug from document

--- src/module/document-model/document-model.controller.ts
+++ src/module/document-model/document-model.controller.ts
@@ -85,7 +85,6 @@ export class DocumentModelController {
     const contentType = this.documentModelService.getContentTypeFromExtension(
       fileStream.path.toString(),
     );
-    console.log(contentType);
     res.setHeader('Content-Type', contentType); // Adjust the content type as needed
     fileStream.pipe(res);
   }
